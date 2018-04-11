4b321fcc79bfce677a7c222f583330abc1932c92
Abelardo Vaje <abelardovaje@B6DE-20150146.local>
2018-04-11 13:18:27 +0800

    update socket listeners

--- bootstrap/sockets.js
+++ bootstrap/sockets.js
@@ -14,5 +14,16 @@ export default io => {
             console.log('NEW_MESSAGE', data);
             socket.broadcast.to('beline:' + data.client.id).emit('action', {type: 'RECEIVE_NEW_MESSAGE', payload: data});
         });
+
+        socket.on('ADD_NEW_CONTACT', function(data) {
+            console.log('ADD_NEW_CONTACT', data);
+            socket.to('beline:' + data.contact.id).emit('action', {type: 'NEW_CONTACT', payload: data});
+        });
+
+        socket.on('DELETE_CONTACT', function(data) {
+            console.log('DELETE_CONTACT', data);
+            socket.to('beline:' + data.contact.id).emit('action', {type: 'DELETE_CONTACTS', payload: data.user});
+        });
+
     });
 };
